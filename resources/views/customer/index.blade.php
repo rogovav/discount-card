a673d2263ac1cbc04232d7a95e1542ad76719cda
alexander <alerogov@gmail.com>
2019-07-16 14:34:24 +0300

    Create XML controller

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -11,12 +11,13 @@
                 <thead>
                 <tr>
                     <th>ФИО</th>
+                    <th>Email</th>
 {{--                    <th>День рождения</th>--}}
                     <th>Телефон</th>
                     <th>Номер карты</th>
                     <th>Сумма</th>
                     <th>Скидка</th>
-                    <th>MAC</th>
+{{--                    <th>MAC</th>--}}
 {{--                    <th>В системе</th>--}}
                 </tr>
                 </thead>
@@ -24,11 +25,12 @@
                     <tr class="search-tr">
                         <td>{{ $customer->name }} {{ $customer->surname }} {{ $customer->patronymic }}</td>
 {{--                        <td>{{ $customer->birthday }}</td>--}}
+                        <td>{{ $customer->email }}</td>
                         <td>{{ $customer->phone }}</td>
                         <td>{{ "KREPM" . $customer->card_number }}</td>
                         <td>{{ $customer->sum }}</td>
                         <td>{{ $customer->percent }}</td>
-                        <td>{{ $customer->MAC }}</td>
+{{--                        <td>{{ $customer->MAC }}</td>--}}
 {{--                        <td>{{ $customer->registered? 'Да' : 'Нет' }}</td>--}}
                     </tr>
                 @endforeach
